2d39f50f64971d2374e5975898cd99e873e022d2
Emanuel Silva <emanuelborgesdasilva@gmail.com>
2018-11-19 15:01:25 -0200

    Update SextilhaInfo.jsx

--- src/components/SextilhaInfo/SextilhaInfo.jsx
+++ src/components/SextilhaInfo/SextilhaInfo.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './SextilhaInfo.css';
-
-
 import PeopleRight  from '../../images/peopleRight.png';
 
 export default class SextilhaInfo extends Component {
