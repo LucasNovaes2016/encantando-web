c7ab6b62c13478c3381810a11adaea23f2c4881e
Emanuel Silva <emanuelborgesdasilva@gmail.com>
2018-11-19 15:03:07 -0200

    Update SextilhaInfo.jsx

--- src/components/SextilhaInfo/SextilhaInfo.jsx
+++ src/components/SextilhaInfo/SextilhaInfo.jsx
@@ -9,18 +9,14 @@ export default class SextilhaInfo extends Component {
             <div className = "main">
 
             	<img  id = "peopleRight"  src = { PeopleRight } alt = ""/>
-
             	<div className = "titleSext">
-					<h1>EXPLICANDO O CORDEL <br /> </h1>
-               
+			<h1>EXPLICANDO O CORDEL <br /> </h1>
                 </div>
-
                 <div className = "info">
                 	<p>A Sextilha é uma estrofe com rimas deslocadas, constituída de seis linhas, 
                 	ou seis versos de sete sílabas, nomes que têm a mesma significação. Na Sextilha, 
                 	rimam as linhas pares entre si, conservando as demais em versos brancos (sem rima obrigatória).</p>
                 </div>
-
             </div>
         )
     }
